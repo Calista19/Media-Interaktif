17c8bad6befc1245073428a417b66dc532264489
Calista19 <clstrmdhn1912@gmail.com>
2022-10-27 07:03:12 -0700

    update terbaru

--- Media Interaktif/JavaScript/materi.js
+++ Media Interaktif/JavaScript/materi.js
@@ -1,22 +1,20 @@
-// $(document).ready(function(){
-//     $.getJSON("../JSON/materi.json", function(data){
-//         console.log(data)
-
-//         $('.title').html(data.title);
-//     })
-// })
-
-function judul(evt, nmrIsi) {
-    var i, isian, pilihan;
+function judul() {
+    var i, isian, pilihan, materian, meteripilihan;
     isian = document.getElementsByClassName("judul");
     for (i = 0; i < isian.length; i++) {
       isian[i].style.display = "none";
     }
+    materian = document.getElementsByClassName("materi");
+    for (i = 0; i < materian.length; i++){
+      materian[i].style.display = "none";
+    }
     pilihan = document.getElementsByClassName("menubar");
     for (i = 0; i < pilihan.length; i++) {
-      pilihan[i].className = pilihan[i].className.replace(" active", "");
+      pilihan[i].className = pilihan[i].className.replace(" menubaractive", "", "");
     }
     document.getElementById(nmrIsi).style.display = "block";
-    evt.currentTarget.className += " active";
+    document.getElementById(mtr).style.display = "block";
+    evt.currentTarget.className += " menubaractive";
+
   }
 
